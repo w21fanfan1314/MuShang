247fc28c6d799aed6b08dde609df76f2c93f7b91
Robin.Liu <w21fanfan1314@live.com>
2018-06-06 16:54:45 +0800

    会员充值功能接口对接

---
+++ core/MSPayment.js
@@ -0,0 +1,38 @@
+import MMBApiList from "MMBApiList.js";
+
+/**
+ * 支付操作
+ */
+export default class MSPayment
+{
+  api = new MMBApiList("MMBLibrary/BLL/StoreValueBLL/");
+  constructor()
+  {
+
+  }
+  /**
+   * 执行会员充值
+   * @params recharge 充值数据
+   */
+  memberPay(recharge)
+  {
+    this.api.payOrder_StoreValue(recharge.id, recharge.money, {
+      onResp: res=>
+      {
+        if (res.code === 1)
+        {
+
+        }
+        else
+        {
+          wx.showModal({
+            title: "会员充值错误",
+            content: res.msg,
+            showCancel: false, 
+            confirmText:'关闭'
+          })
+        }
+      }
+    });
+  }
+}
